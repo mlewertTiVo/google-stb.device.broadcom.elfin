23272bad5fb413d1a7b72739dcd7d627f976ee84
Pierre Couillaud <pierre@broadcom.com>
2015-06-16 06:38:24 -0700

    avko: initial device setup.
    refs #SWANDROID-2346
    
    Change-Id: Ifa65a0c15d2df66d08aee91699f834110c761973
    Signed-off-by: Pierre Couillaud <pierre@broadcom.com>

---
+++ AndroidProducts.mk
@@ -0,0 +1,8 @@
+# copyright 2015 - broadcom canada ltd.
+#
+# warning: auto-generated module, edit at your own risks...
+#
+
+# this configuration is for device avko
+
+PRODUCT_MAKEFILES := $(LOCAL_DIR)/full_avko.mk
---
+++ vendorsetup.sh
@@ -0,0 +1,8 @@
+# copyright 2015 - broadcom canada ltd.
+#
+# warning: auto-generated module, edit at your own risks...
+#
+
+# this configuration is for device avko
+
+add_lunch_combo full_avko-eng
