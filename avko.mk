d5a94864907a6cf0af9c9b2daabea2aeaececaf3
Pierre Couillaud <pierre@broadcom.com>
2015-12-08 19:37:10 -0800

    device: include tv codecs.
    refs #SWANDROID-2362
    
    Change-Id: Ie9a02ee83c243c3f787408d6a7076d314d957b5e
    Signed-off-by: Pierre Couillaud <pierre@broadcom.com>

--- avko.mk
+++ avko.mk
@@ -90,6 +90,7 @@ PRODUCT_COPY_FILES += \
     device/google/avko/audio_policy_btusb.conf:system/etc/audio_policy.conf \
     frameworks/av/media/libstagefright/data/media_codecs_google_audio.xml:system/etc/media_codecs_google_audio.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:system/etc/media_codecs_google_video.xml \
+    frameworks/av/media/libstagefright/data/media_codecs_google_tv.xml:system/etc/media_codecs_google_tv.xml \
     device/google/avko/webview-command-line:/data/local/tmp/webview-command-line \
     frameworks/native/data/etc/android.hardware.ethernet.xml:system/etc/permissions/android.hardware.ethernet.xml \
     frameworks/native/data/etc/android.hardware.hdmi.cec.xml:system/etc/permissions/android.hardware.hdmi.cec.xml \
