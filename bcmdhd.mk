d0d251ca08b2a58bb7ac9df6cba0f6ef78d09d5f
Pierre Couillaud <pierre.couillaud@broadcom.com>
2016-03-19 09:10:31 -0700

    bcmdhd: fixedup 'tab-to-space' overide.
    refs #SWANDROID-2720
    
    Change-Id: Icdf90aa9d28bc28662b764744b75494476da22f5
    Signed-off-by: Pierre Couillaud <pierre.couillaud@broadcom.com>

--- bcmdhd.mk
+++ bcmdhd.mk
@@ -6,21 +6,21 @@ BRCM_DHD_NVRAM_DIR  ?= ${BROADCOM_DHD_SOURCE_PATH}/nvrams
 BRCM_DHD_NVRAM_NAME ?= bcm43570.nvm
 
 ${B_DHD_OBJ_ROOT}:
-   mkdir -p ${B_DHD_OBJ_ROOT}
+	mkdir -p ${B_DHD_OBJ_ROOT}
 
 ${B_DHD_OBJ_ROOT}/driver/bcmdhd.ko: build_kernel ${B_DHD_OBJ_ROOT}
-   cp -faR ${BROADCOM_DHD_SOURCE_PATH}/dhd ${B_DHD_OBJ_ROOT} && cp ${BROADCOM_DHD_SOURCE_PATH}/*.sh ${B_DHD_OBJ_ROOT}
-   +cd ${B_DHD_OBJ_ROOT} && source ./setenv-android-stb7445.sh ${BROADCOM_WIFI_CHIPSET} && ./bfd-drv-cfg80211.sh
+	cp -faR ${BROADCOM_DHD_SOURCE_PATH}/dhd ${B_DHD_OBJ_ROOT} && cp ${BROADCOM_DHD_SOURCE_PATH}/*.sh ${B_DHD_OBJ_ROOT}
+	+cd ${B_DHD_OBJ_ROOT} && source ./setenv-android-stb7445.sh ${BROADCOM_WIFI_CHIPSET} && ./bfd-drv-cfg80211.sh
 
 ${B_DHD_OBJ_ROOT}/fw.bin.trx: ${BROADCOM_DHD_SOURCE_PATH}/firmware/${BROADCOM_WIFI_CHIPSET}-roml/${BRCM_DHD_FW_NAME} ${B_DHD_OBJ_ROOT}
-   cp -p $< $@
+	cp -p $< $@
 
 ${B_DHD_OBJ_ROOT}/nvm.txt: ${BRCM_DHD_NVRAM_DIR}/${BRCM_DHD_NVRAM_NAME} ${B_DHD_OBJ_ROOT}
-   cp -p $< $@
+	cp -p $< $@
 
 .PHONY: brcm_dhd_driver
 brcm_dhd_driver: ${BRCM_DHD_DRIVER_TARGETS}
-   @echo "'brcm_dhd_driver' targets: ${BRCM_DHD_DRIVER_TARGETS}"
+	@echo "'brcm_dhd_driver' targets: ${BRCM_DHD_DRIVER_TARGETS}"
 
 .PHONY: clean_brcm_dhd_driver
 clean_brcm_dhd_driver:
