06f3a0436b55d2a0f1b97564c152e8de8ff431b2
Pierre Couillaud <pierre@broadcom.com>
2016-08-12 14:59:43 -0700

    avko: fix up OTA based builds.
    refs #SWANDROID-3452
    
    Change-Id: Ibcbf6fa8d14c28b9c0d11d9bf305d1cd3e40b6d0
    Signed-off-by: Pierre Couillaud <pierre@broadcom.com>

--- AndroidBoard.mk
+++ AndroidBoard.mk
@@ -19,7 +19,6 @@ LOCAL_PATH := $(my-dir)
 include $(LOCAL_PATH)/AndroidKernel.mk
 include $(LOCAL_PATH)/AndroidPhony.mk
 
-# adding bolt and bsu to the output target package for OTA packaging
-$(call add-radio-file, bolt-bb.bin)
-$(call add-radio-file, android_bsu.elf)
 $(call add-radio-file, bootloader.img)
+$(call add-radio-file, gpt.bin)
+
--- AndroidPhony.mk
+++ AndroidPhony.mk
@@ -1,9 +1,5 @@
-
-.PHONY: android_bsu.elf
-android_bsu.elf: build_bootloaderimg
-
-.PHONY: bolt-bb.bin
-bolt-bb.bin: build_bootloaderimg
-
 .PHONY: bootloader.img
 bootloader.img: build_bootloaderimg
+
+.PHONY: gpt.bin
+gpt.bin: makegpt
