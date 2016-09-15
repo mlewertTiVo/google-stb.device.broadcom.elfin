dbbf11ae6c17bc47d4d7dfa2358471a4568fb175
Pierre Couillaud <pierre.couillaud@broadcom.com>
2016-09-15 14:37:37 -0700

    avko: relocate 'hwcfg' for forward compatibility.
    - we relocate the 'hwcfg' at rootfs.
    - use Android.mk facility to setup 'hwcfg' in the ramdisk/rootfs,
    setup sepolicy for it.
    - provide ability to override dynamically the location when using
    bcmdhd.
    
    refs #SWANDROID-2980
    
    Change-Id: Ia581eac75871312878c8d7f700bc28ee8687424a
    Signed-off-by: Pierre Couillaud <pierre.couillaud@broadcom.com>

--- bcmdhd.mk
+++ bcmdhd.mk
@@ -4,13 +4,14 @@ BRCM_DHD_KO_NAME    := bcmdhd.ko
 BRCM_DHD_FW_NAME    ?= pcie-ag-pktctx-splitrx-amsdutx-txbf-p2p-mchan-idauth-idsup-tdls-mfp-sr-proptxstatus-pktfilter-wowlpf-ampduhostreorder-keepalive-wfds.bin
 BRCM_DHD_NVRAM_DIR  ?= ${BROADCOM_DHD_SOURCE_PATH}/nvrams
 BRCM_DHD_NVRAM_NAME ?= bcm43570.nvm
+BRCM_DHD_TARGET_NVRAM_PATH := "/hwcfg/nvm.txt"
 
 ${B_DHD_OBJ_ROOT}:
 	mkdir -p ${B_DHD_OBJ_ROOT}
 
 ${B_DHD_OBJ_ROOT}/driver/bcmdhd.ko: build_kernel ${B_DHD_OBJ_ROOT}
 	cp -faR ${BROADCOM_DHD_SOURCE_PATH}/dhd ${B_DHD_OBJ_ROOT} && cp ${BROADCOM_DHD_SOURCE_PATH}/*.sh ${B_DHD_OBJ_ROOT}
-	cd ${B_DHD_OBJ_ROOT} && source ./setenv-android-stb7445.sh ${BROADCOM_WIFI_CHIPSET} && ./bfd-drv-cfg80211.sh
+	cd ${B_DHD_OBJ_ROOT} && source ./setenv-android-stb7445.sh ${BROADCOM_WIFI_CHIPSET} && BRCM_DHD_TARGET_NVRAM_PATH=${BRCM_DHD_TARGET_NVRAM_PATH} ./bfd-drv-cfg80211.sh
 
 ${B_DHD_OBJ_ROOT}/fw.bin.trx: ${BROADCOM_DHD_SOURCE_PATH}/firmware/${BROADCOM_WIFI_CHIPSET}-roml/${BRCM_DHD_FW_NAME} ${B_DHD_OBJ_ROOT}
 	cp -p $< $@
