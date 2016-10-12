2d867296fc3c146c58e79164e0f1a9faa9e6d200
Pierre Couillaud <pierre.couillaud@broadcom.com>
2016-10-12 07:54:15 -0700

    avko: switch all platforms to 4.1 kernel.
    refs #SWANDROID-3556
    
    Change-Id: I51718691e53f0aa0e263ff088a502d6df5ad0f18
    Signed-off-by: Pierre Couillaud <pierre.couillaud@broadcom.com>

--- avko.mk
+++ avko.mk
@@ -83,6 +83,7 @@ PRODUCT_COPY_FILES += \
     device/broadcom/avko/bootanimation.zip:system/media/bootanimation.zip \
     device/broadcom/avko/init.blockdev.rc:root/init.blockdev.rc \
     device/broadcom/avko/init.blockdev.rc:root/init.recovery.blockdev.rc \
+    device/broadcom/avko/init.broadcomstb.usb.configfs.rc:root/init.bcm_platform.usb.rc \
     device/broadcom/avko/init.eth.rc:root/init.eth.rc \
     device/broadcom/avko/init.recovery.bcm_platform.rc:root/init.recovery.avko.rc \
     device/broadcom/avko/media_codecs.xml:system/etc/media_codecs.xml \
@@ -108,7 +109,6 @@ PRODUCT_COPY_FILES += \
     ${BCM_VENDOR_STB_ROOT}/bcm_platform/cfgs/gps.conf:system/etc/gps.conf \
     ${BCM_VENDOR_STB_ROOT}/bcm_platform/cfgs/init.broadcomstb.rc:root/init.avko.rc \
     ${BCM_VENDOR_STB_ROOT}/bcm_platform/cfgs/init.broadcomstb.fs.rc:root/init.brcm_fs.rc \
-    ${BCM_VENDOR_STB_ROOT}/bcm_platform/cfgs/init.broadcomstb.usb.rc:root/init.bcm_platform.usb.rc \
     ${BCM_VENDOR_STB_ROOT}/bcm_platform/cfgs/init.nx.dynheap.rc:root/init.nx.dynheap.rc \
     ${BCM_VENDOR_STB_ROOT}/bcm_platform/cfgs/ueventd.bcm_platform.rc:root/ueventd.avko.rc \
     ${NEXUS_BIN_DIR}/droid_pm.ko:system/vendor/drivers/droid_pm.ko \
