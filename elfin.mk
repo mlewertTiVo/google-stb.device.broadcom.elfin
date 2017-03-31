11ed77697a2e7c892407a0c41af6c97ca50c306d
Pierre Couillaud <pierre.couillaud@broadcom.com>
2017-03-31 11:55:26 -0700

    gpio_keys_polled: rename module to match device name.
    refs #SWANDROID-3918
    
    Change-Id: I7610054dee4aba459739d5783754fd57186760b4
    Signed-off-by: Pierre Couillaud <pierre.couillaud@broadcom.com>

--- elfin.mk
+++ elfin.mk
@@ -38,7 +38,7 @@ export LOCAL_DEVICE_MEDIA
 export LOCAL_DEVICE_OVERLAY      := device/broadcom/elfin/overlay
 export LOCAL_DEVICE_SEPOLICY_BLOCK := device/broadcom/elfin/sepolicy-block
 export LOCAL_DEVICE_AON_GPIO     := device/broadcom/elfin/aon_gpio.cfg:system/vendor/power/aon_gpio.cfg
-export LOCAL_DEVICE_KEY_POLL     := device/broadcom/common/keylayout/gpio_keys_polled.kl:system/usr/keylayout/gpio_keys_polled_5.kl
+export LOCAL_DEVICE_KEY_POLL     := device/broadcom/common/keylayout/gpio_keys_polled.kl:system/usr/keylayout/gpio_keys_polled.kl
 export LOCAL_DEVICE_BT_CONFIG    := device/broadcom/elfin/bluetooth/vnd_elfin.txt
 export LOCAL_DEVICE_USERDATA     := 5927582720 # ~5.52GB
 export LOCAL_DEVICE_GPT          := device/broadcom/common/gpts/ab-u.conf
