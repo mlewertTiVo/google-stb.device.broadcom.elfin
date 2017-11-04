016008168a4f6146546f77eebe53f9488f058d10
Pierre Couillaud <pierre.couillaud@broadcom.com>
2017-11-04 14:41:24 -0700

    elfin_l: elfin full treble device without a|b-mode support (legacy).
    refs #SWANDROID-4538
    b/68717936
    
    Change-Id: I5e25c12db6442ba5f647b4e84db6ebc7d75c37a2
    Signed-off-by: Pierre Couillaud <pierre.couillaud@broadcom.com>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,4 +1,5 @@
 PRODUCT_MAKEFILES := $(LOCAL_DIR)/elfin.mk
 PRODUCT_MAKEFILES += $(LOCAL_DIR)/elfin_nvi.mk
 PRODUCT_MAKEFILES += $(LOCAL_DIR)/elfin_mini.mk
+PRODUCT_MAKEFILES += $(LOCAL_DIR)/elfin_l.mk
 PRODUCT_MAKEFILES += $(LOCAL_DIR)/aosp_elfin.mk
