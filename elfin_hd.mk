f0ea9dad762438dc6bf0f707504b7de266465cc9
Pierre Couillaud <pierre.couillaud@broadcom.com>
2017-12-03 09:20:27 -0800

    elfin_[mini|hd]: profiles for 1GB device experiments.
    - "_hd" is elfin with 1GB, 1080p60 max decoder and 1080p graphics
      (formerly known as elfin_mini).
    - "_mini" is elfin with 1GB
      (therefore 4K decode and 1080p graphics).
    
    refs #SWANDROID-4612
    
    Change-Id: I2dcf8e8f814fea6d87777f143dfccca801d14b8a
    Signed-off-by: Pierre Couillaud <pierre.couillaud@broadcom.com>
    (cherry picked from commit 56dca713744bec915f7256f5823e334f09de8b0e)

--- elfin_hd.mk
+++ elfin_hd.mk
@@ -1,37 +1,37 @@
-export ANDROID_PRODUCT_OUT       := elfin_mini
+export ANDROID_PRODUCT_OUT       := elfin_hd
 export LOCAL_DEVICE_FULL_TREBLE  := y
 
 LOCAL_DEVICE_FSTAB               := device/broadcom/elfin/fstab/fstab.verity.ab-update.early.zram:root/fstab.bcm
-LOCAL_DEVICE_FSTAB               += device/broadcom/elfin/fstab/fstab.verity.ab-update.early.zram:root/fstab.elfin_mini
+LOCAL_DEVICE_FSTAB               += device/broadcom/elfin/fstab/fstab.verity.ab-update.early.zram:root/fstab.elfin_hd
 export LOCAL_DEVICE_FSTAB
 
 export LOCAL_DEVICE_GPT          := device/broadcom/common/gpts/ab-u.conf
 
-LOCAL_DEVICE_RCS                 := device/broadcom/common/rcs/init.rc:root/init.elfin_mini.rc
-LOCAL_DEVICE_RCS                 += device/broadcom/common/rcs/ueventd.rc:root/ueventd.elfin_mini.rc
+LOCAL_DEVICE_RCS                 := device/broadcom/common/rcs/init.rc:root/init.elfin_hd.rc
+LOCAL_DEVICE_RCS                 += device/broadcom/common/rcs/ueventd.rc:root/ueventd.elfin_hd.rc
 LOCAL_DEVICE_RCS                 += device/broadcom/elfin/rcs/init.block-zram.rc:root/init.block.rc # block devices
 
-LOCAL_DEVICE_RECOVERY_RCS        := device/broadcom/common/rcs/init.recovery.rc:root/init.recovery.elfin_mini.rc
+LOCAL_DEVICE_RECOVERY_RCS        := device/broadcom/common/rcs/init.recovery.rc:root/init.recovery.elfin_hd.rc
 LOCAL_DEVICE_RECOVERY_RCS        += device/broadcom/elfin/rcs/init.block-zram.rc:root/init.recovery.block.rc # block devices
 
 # kernel command line.
 LOCAL_DEVICE_KERNEL_CMDLINE      := mem=1024m@0m
-LOCAL_DEVICE_KERNEL_CMDLINE      += bmem=452m@540m
-LOCAL_DEVICE_KERNEL_CMDLINE      += brcm_cma=224m@316m
+LOCAL_DEVICE_KERNEL_CMDLINE      += bmem=243m@736m
+LOCAL_DEVICE_KERNEL_CMDLINE      += brcm_cma=224m@512m
 LOCAL_DEVICE_KERNEL_CMDLINE      += rootwait init=/init ro
 export LOCAL_DEVICE_KERNEL_CMDLINE
 
 # compile the media codecs for the device.
 LOCAL_DEVICE_MEDIA               := device/broadcom/common/media/media_codecs_hd.xml:system/etc/media_codecs.xml
 LOCAL_DEVICE_MEDIA               += device/broadcom/common/media/media_profiles.xml:system/etc/media_profiles.xml
-LOCAL_DEVICE_MEDIA               += device/broadcom/elfin/media_codecs_performance.xml:system/etc/media_codecs_performance.xml
+LOCAL_DEVICE_MEDIA               += device/broadcom/elfin/media_codecs_performance_hd.xml:system/etc/media_codecs_performance.xml
 export LOCAL_DEVICE_MEDIA
 
 LOCAL_DEVICE_DALVIK_CONFIG       := frameworks/native/build/tablet-7in-hdpi-1024-dalvik-heap.mk
 export LOCAL_DEVICE_DALVIK_CONFIG
 
 export HW_AB_UPDATE_SUPPORT      := y
-export LOCAL_DEVICE_OVERLAY      := device/broadcom/elfin/overlay_mini
+export LOCAL_DEVICE_OVERLAY      := device/broadcom/elfin/overlay_hd
 
 # common to all elfin devices.
 include device/broadcom/elfin/common.mk
@@ -39,11 +39,11 @@ include device/broadcom/elfin/common.mk
 # baseline the common support.
 $(call inherit-product, device/broadcom/common/bcm.mk)
 #$(call inherit-product, build/make/target/product/product_launched_with_n.mk)
-PRODUCT_NAME                     := elfin_mini
-PRODUCT_MODEL                    := elfin_mini
+PRODUCT_NAME                     := elfin_hd
+PRODUCT_MODEL                    := elfin_hd
 PRODUCT_BRAND                    := google
-PRODUCT_DEVICE                   := elfin_mini
+PRODUCT_DEVICE                   := elfin_hd
 
 # additional setup per device.
-PRODUCT_PROPERTY_OVERRIDES    += ro.hardware=elfin_mini
-TARGET_BOOTLOADER_BOARD_NAME  := elfin_mini
+PRODUCT_PROPERTY_OVERRIDES    += ro.hardware=elfin_hd
+TARGET_BOOTLOADER_BOARD_NAME  := elfin_hd
