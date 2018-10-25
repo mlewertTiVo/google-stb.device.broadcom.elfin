7c14bba0d586b863472bed7e2b6120d7919e4da5
Pierre Couillaud <pierre.couillaud@broadcom.com>
2018-10-25 12:23:39 -0700

    elfin: enable force scan mode to 1080p.
    refs #SWANDROID-5656
    
    Change-Id: I7bb8114d02e078b66e51f59da7f488b2e28e2687
    Signed-off-by: Pierre Couillaud <pierre.couillaud@broadcom.com>

--- elfin.mk
+++ elfin.mk
@@ -87,7 +87,9 @@ PRODUCT_PROPERTY_OVERRIDES += \
    \
    ro.nx.eth.irq_mode_mask=3:2 \
    \
-   ro.com.google.clientidbase=android-elfin-tv
+   ro.com.google.clientidbase=android-elfin-tv \
+   \
+   ro.nx.media.vdec.fsm1080p=1
 
 ifeq ($(BDSP_MS12_SUPPORT),D)
 PRODUCT_PROPERTY_OVERRIDES += \
